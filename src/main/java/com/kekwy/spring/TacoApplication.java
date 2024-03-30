431c30790e8e39eb150c80488bede01edbce9588
Kekwy <keekkewy@qq.com>
2024-03-30 13:31:26 +0800

    initial

---
+++ src/main/java/com/kekwy/spring/TacoApplication.java
@@ -0,0 +1,13 @@
+package com.kekwy.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoApplication.class, args);
+    }
+
+}
